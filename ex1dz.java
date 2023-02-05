76502084ff5a277de337289aa094a41e12a8ea38
Savenok <Romasav2017@gmail.com>
2023-02-05 12:17:56 +0300

    11

---
+++ ex1dz.java
@@ -0,0 +1,19 @@
+﻿// Пусть дан LinkedList с несколькими элементами. Реализуйте метод, который вернет “перевернутый” список.
+import java.util.LinkedList;
+public class ex1dz{
+    public static void main(String[] arg){
+        LinkedList linklist = new LinkedList<>();
+        linklist.add("X");
+        linklist.add("e");
+        linklist.add("n");
+        linklist.add("o");
+        for (int index = 0; index < linklist.size(); index++) {
+            System.out.print(linklist.get(index));
+        }
+        System.out.println(" ");
+        for (int i = linklist.size()-1; i > -1; i--) {
+            System.out.print(linklist.get(i));   
+        }
+
+    }
+}
